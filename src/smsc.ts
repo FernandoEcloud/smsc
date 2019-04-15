0248e548218695ec9b0224a32c211b17733e2de3
Fernando Olivero <fernando.olivero@ecloudsolutions.com>
2019-04-15 15:59:26 -0300

    REVERT

--- src/smsc.ts
+++ src/smsc.ts
@@ -2,22 +2,22 @@ class Smsc {
   /**
    * @var string ApiKey de SMSC
    */
-  apikey;
+  private apikey;
   /**
    * @var string Alias de SMSC
    */
-  alias;
-  version = '0.3';
-  protocol = 'https';
+  private alias;
+  public version = '0.3';
+  public protocol = 'https';
 
-  priority = null;
-  line = null;
-  mensaje;
-  numeros = [];
-  return;
+  private priority = null;
+  private line = null;
+  private mensaje;
+  private numeros = [];
+  private return;
 
 
-  constructor(alias = null, apikey = null) {
+  public constructor(alias = null, apikey = null) {
     if (alias !== null) {
       this.setAlias(alias);
     }
@@ -27,28 +27,28 @@ class Smsc {
   }
 
 
-  getApikey = () => {
+  public getApikey = () => {
     return this.apikey;
   }
-  setApikey = (apikey) => {
+  public setApikey = (apikey) => {
     this.apikey = apikey;
   }
-  getAlias = () => {
+  public getAlias = () => {
     return this.alias;
   }
-  setAlias = (alias) => {
+  public setAlias = (alias) => {
     this.alias = alias;
   }
-  getData = () => {
+  public getData = () => {
     return this.return['data'];
   }
-  getStatusCode = () => {
+  public getStatusCode = () => {
     return this.return['code'];
   }
-  getStatusMessage = () => {
+  public getStatusMessage = () => {
     return this.return['message'];
   }
-  exec = (cmd = null, extradata = null) => {
+  public exec = (cmd = null, extradata = null) => {
     const THIS = this;
     THIS.return = null;
     // construyo la URL de consulta
@@ -84,7 +84,7 @@ class Smsc {
    * Estado del sistema SMSC.
    * @return bool Devuelve true si no hay demoras en la entrega.
    */
-  getEstado = () => {
+  public getEstado = () => {
     let ret = this.exec('estado');
     if (!ret)
       return false;
@@ -100,7 +100,7 @@ class Smsc {
    * Validar número
    * @return bool Devuelve true si es un número válido.
    */
-  evalNumero = (prefijo, fijo = null) => {
+  public evalNumero = (prefijo, fijo = null) => {
     let ret = this.exec('evalnumero', '&num=' + prefijo + (fijo === null ? '' : '-' + fijo));
     if (!ret)
       return false;
@@ -116,7 +116,7 @@ class Smsc {
    *
    * @return array
    */
-  getSaldo = () => {
+  public getSaldo = () => {
     let ret = this.exec('saldo');
     if (!ret)
       return false;
@@ -133,7 +133,7 @@ class Smsc {
    * @param int $prioridad 0:todos 1:baja 2:media 3:alta
    * @return array
    */
-  getEncolados = (prioridad = 0) => {
+  public getEncolados = (prioridad = 0) => {
     let ret = this.exec('encolados', '&prioridad=' + +prioridad);
     if (!ret)
       return false;
@@ -157,40 +157,40 @@ class Smsc {
    *                    Si sólo especifica prefijo, se tomará como número completo (no recomendado).
    *                    Ej: 530000
    */
-  addNumero = (prefijo, fijo = null) => {
+  public addNumero = (prefijo, fijo = null) => {
     if (fijo === null) {
       this.numeros.push(prefijo);
     } else {
       this.numeros.push(prefijo + '-' + fijo);
     }
   }
-  getMensaje = () => {
+  public getMensaje = () => {
     return this.mensaje;
   }
-  setMensaje = (mensaje) => {
+  public setMensaje = (mensaje) => {
     this.mensaje = mensaje;
   }
 
-  getLinea = () => {
+  public getLinea = () => {
     return this.line;
   }
   /**
    * @param int $line_id. Only for dedicated lines.
    */
-  setLinea = (line_id) => {
+  public setLinea = (line_id) => {
     this.line = line_id;
   }
-  getPrioridad = () => {
+  public getPrioridad = () => {
     return this.line;
   }
   /**
    * @param int $priority 1 for low to 7 for high. null for default.
    */
-  setPrioridad = (priority) => {
+  public setPrioridad = (priority) => {
     this.priority = priority;
   }
 
-  enviar = () => {
+  public enviar = () => {
     let params = [];
     params.push(`num=${this.numeros.join(",")}`);
     params.push(`msj=${encodeURI(this.mensaje)}`);
@@ -225,7 +225,7 @@ class Smsc {
    * más nuevos al sms con id especificado (acelera la
    * consulta y permite un chequeo rápido de nuevos mensajes)
    */
-  getRecibidos = ($ultimoid = 0) => {
+  public getRecibidos = ($ultimoid = 0) => {
     const ret = this.exec('recibidos', `&ultimoid=${+$ultimoid}`);
     if (!ret)
       return false;
